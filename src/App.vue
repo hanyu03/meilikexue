6ddc317b7beeab871637e9f9c835de2ecec71da4
hanyu03 <1136493544@qq.com>
2017-11-01 13:09:07 +0800

    router-link激活样式

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-header>
+        <HeaderNav></HeaderNav>
+      </el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
+import HeaderNav from '@/components/header/HeaderNav'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    HeaderNav
+  }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  -moz-osx-font-smoothing: grayscale; */
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/header/HeaderNav.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="nav">
+      <el-row type="flex" align="middle">
+        <el-col class="logo" :span="2">
+          <img src="../../assets/images/nav_logo.png" alt="">
+        </el-col>
+        <el-col :span="21">
+          <div></div>
+        </el-col>
+        <el-col :span="1">
+          <div></div>
+        </el-col>
+      </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+/* .nav {
+  width:100%;
+  height:100%;
+} */
+</style>
